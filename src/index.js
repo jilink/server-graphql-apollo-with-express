9996c148a997640775137df5f3575f1ed3f5f969
jilink <jimmyfac1@gmail.com>
2020-11-28 17:20:55 +0100

    Mutation: Create & Delete messages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
@@ -42,6 +43,11 @@ const schema = gql`
     message(id: ID!): Message!
   }
 
+  type Mutation {
+    createMessage(text: String!): Message!
+    deleteMessage(id: ID!): Boolean!
+  }
+
   type User {
     id: ID!
     username: String!
@@ -70,6 +76,32 @@ const resolvers = {
     },
     message: (parent, { id }) => {
       return messages[id];
+    },
+  },
+
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuidv4();
+      const message = {
+        id,
+        text,
+        userId: me.id,
+      };
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+      return message;
+    },
+
+    deleteMessage: (parent, { id }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return false;
+      }
+
+      messages = otherMessages;
+
+      return true;
     },
   },
 
