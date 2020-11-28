7b24caf7c2dd915ee2b03ce2671aea6e2d51ec7c
jilink <jimmyfac1@gmail.com>
2020-11-28 16:06:40 +0100

    Basic schema & resolver

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
+import cors from "cors";
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
 
 const app = express();
 
+app.use(cors());
+
+let users = {
+  1: {
+    id: "1",
+    username: "Jimmy Soussan",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davis",
+  },
+};
+
+const me = users[1];
+
 const schema = gql`
   type Query {
+    users: [User!]
     me: User
+    user(id: ID!): User
   }
 
   type User {
-    usernmae: String!
+    id: ID!
+    username: String!
   }
 `;
 const resolvers = {
   Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
     me: () => {
-      return {
-        username: "Robin wieruch",
-      };
+      return me;
     },
   },
 };
