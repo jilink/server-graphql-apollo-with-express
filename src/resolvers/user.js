5950cff9fbfd1a72f003b3041fdfe5267f721fe9
jilink <jimmyfac1@gmail.com>
2020-11-30 18:17:20 +0100

    SignIn

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { AuthenticationError, UserInputError } from "apollo-server";
 
 const createToken = async (user, secret, expiresIn) => {
   const { id, email, username } = user;
@@ -33,6 +34,18 @@ export default {
         password,
       });
 
+      return { token: createToken(user, secret, "30m") };
+    },
+
+    signIn: async (parent, { login, password }, { models, secret }) => {
+      const user = await models.User.findByLogin(login);
+      if (!user) {
+        throw new UserInputError("No user found with this login credentials");
+      }
+      const isValid = await user.validatePassword(password);
+      if (!isValid) {
+        throw new UserInputError("Invalid password");
+      }
       return { token: createToken(user, secret, "30m") };
     },
   },
