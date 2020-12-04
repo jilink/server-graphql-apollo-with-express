109beefca41c22868c152707baec81aa518133e8
jilink <jimmyfac1@gmail.com>
2020-12-04 16:57:43 +0100

    PagInfo Endcursor & hasNExtPage

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -5,11 +5,24 @@ import Sequelize from "sequelize";
 export default {
   Query: {
     messages: async (parent, { cursor, limit = 100 }, { models }) => {
-      return await models.Message.findAll({
+      const cursorOptions = cursor
+        ? { where: { createdAt: { [Sequelize.Op.lt]: cursor } } }
+        : {};
+      const messages = await models.Message.findAll({
         order: [["createdAt", "DESC"]],
-        limit,
-        where: cursor ? { createdAt: { [Sequelize.Op.lt]: cursor } } : null,
+        limit: limit + 1,
+        ...cursorOptions,
       });
+      const hasNextPage = messages.length > limit;
+      const edges = hasNextPage ? messages.slice(0, -1) : messages;
+
+      return {
+        edges,
+        pageInfo: {
+          hasNextPage,
+          endCursor: messages[messages.length - 1].createdAt,
+        },
+      };
     },
     message: async (parent, { id }, { models }) => {
       return await models.Message.findByPk(id);
