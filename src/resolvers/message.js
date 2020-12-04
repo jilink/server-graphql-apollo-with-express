03a18810a910fae14c19236c8f85e5aba0219a63
jilink <jimmyfac1@gmail.com>
2020-12-04 16:28:16 +0100

    offset-limit pagination

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -3,8 +3,8 @@ import { isAuthenticated, isMessageOwner } from "./authorization";
 
 export default {
   Query: {
-    messages: async (parent, args, { models }) => {
-      return await models.Message.findAll();
+    messages: async (parent, { offset = 0, limit = 0 }, { models }) => {
+      return await models.Message.findAll({ offset, limit });
     },
     message: async (parent, { id }, { models }) => {
       return await models.Message.findByPk(id);
